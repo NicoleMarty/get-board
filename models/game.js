f2b0831c7f64b27450c5b96db6d210848726bd49
Nicole Marty <nmarty310@gmail.com>
2020-01-09 11:03:35 -0600

    added useNewUrlParser: true to game.js in models

--- models/game.js
+++ models/game.js
@@ -1,39 +1,42 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true })
+const Schema = mongoose.Schema
 
 const gameSchema = new Schema({
-    image: String,
-    name: {
-        type: String,
-        required: true
-    },
-    minPlayers: {
-        type: Number,
-        required: true
-    },
-    maxPlayers: Number,
-    minPlaytime: {
-        type: Number,
-        required: true
-    },
-    maxPlaytime: Number,
-    minAge: {
-        type: Number,
-        required: true
-    },
-    rating: Schema.Types.Decimal128,
-    rules: String,
-    complexity: String,
-    userId: {
-        type: String,
-        required: true
-    },
-    houseRules: [{
-        rule: String,
-        description: String
-    }]
-});
+  image: String,
+  name: {
+    type: String,
+    required: true
+  },
+  minPlayers: {
+    type: Number,
+    required: true
+  },
+  maxPlayers: Number,
+  minPlaytime: {
+    type: Number,
+    required: true
+  },
+  maxPlaytime: Number,
+  minAge: {
+    type: Number,
+    required: true
+  },
+  rating: Schema.Types.Decimal128,
+  rules: String,
+  complexity: String,
+  userId: {
+    type: String,
+    required: true
+  },
+  houseRules: [
+    {
+      rule: String,
+      description: String
+    }
+  ]
+})
 
-const Game = mongoose.model("Game", gameSchema);
+const Game = mongoose.model('Game', gameSchema)
 
-module.exports = Game;
+module.exports = Game
